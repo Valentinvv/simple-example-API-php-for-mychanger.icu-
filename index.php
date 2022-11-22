29f6baed279efa2eb359eccdc45d5c6cee69700d
Valentinvv <project705@yandex.ru>
2022-11-22 17:30:41 +0200

    add changes

--- index.php
+++ index.php
@@ -23,7 +23,9 @@ if (function_exists('curl_init')) {
 	$ch = -1;
 
 if ($_POST['action'] == 'GET_BALANCE'){
-	$data = get_data_auth();
+	$data[0] = $_POST['url_id'];
+	$data[1] = $_POST['API_id'];
+	$data[2] = $_POST['store_id'];
 	$r = array(
 		'action' => 'GET_BALANCE',
 		'm_shop' => $data[2]
@@ -39,7 +41,9 @@ if ($_POST['action'] == 'GET_BALANCE'){
 }
 
 if ($_POST['action'] == 'check_oper'){
-	$data = get_data_auth();
+	$data[0] = $_POST['url_id'];
+	$data[1] = $_POST['API_id'];
+	$data[2] = $_POST['store_id'];
 	$r = array(
 		'action' => 'check_oper',
 		'm_shop' => $data[2],
@@ -56,7 +60,9 @@ if ($_POST['action'] == 'check_oper'){
 }
 
 if ($_POST['action'] == 'GET_STATUS'){
-	$data = get_data_auth();
+	$data[0] = $_POST['url_id'];
+	$data[1] = $_POST['API_id'];
+	$data[2] = $_POST['store_id'];
 	$r = array(
 		'action' => 'GET_STATUS',
 		'm_shop' => $data[2],
@@ -74,7 +80,9 @@ if ($_POST['action'] == 'GET_STATUS'){
 }
 
 if ($_POST['action'] == 'GET_BALANCE'){
-	$data = get_data_auth();
+	$data[0] = $_POST['url_id'];
+	$data[1] = $_POST['API_id'];
+	$data[2] = $_POST['store_id'];
 	$r = array(
 		'action' => 'GET_BALANCE',
 		'm_shop' => $data[2]
@@ -91,7 +99,9 @@ if ($_POST['action'] == 'GET_BALANCE'){
 }
 
 if ($_POST['action'] == 'Withdraw'){
-	$data = get_data_auth();
+	$data[0] = $_POST['url_id'];
+	$data[1] = $_POST['API_id'];
+	$data[2] = $_POST['store_id'];
 	$r = array(
 		'action' => 'Withdraw',
 		'm_shop' => $data[2],
@@ -112,24 +122,61 @@ if ($_POST['action'] == 'Withdraw'){
 }
 
 if ($_POST['action'] == 'prepareSCI'){
-	$data = get_data_auth();
+	$data[0] = $_POST['url_id'];
+	$data[1] = $_POST['API_id'];
+	$data[2] = $_POST['store_id'];
 	if (!$_POST['desc']) $_POST['desc']='SUPER';
-	$r = array(
-		'action' => 'prepareSCI',
-		'm_shop' => $data[2],
-		'm_order_id' => $_POST['order_id'],
-		'm_amount' => $_POST['sum'],
-		'm_curr' => 'SBR',		
-		'm_desc' => $_POST['desc']
-		);
-	$o1 = implode(':', $r + array($data[1]));       
-	$sc = md5($o1);		
-	$r['m_sign'] = md5($o1);
-	$data_send = json_encode($r);	
-	$res = json_decode(inet_request($data[0],$data_send)); 
-	echo '<pre>'; 
-	print_r($res);
-	echo '</pre>'; 	
+	$a = $_POST;
+	if ($a['colvo'] > 1){
+		for ($i = 1; $i <= $a['colvo']; $i++) {
+			$orderid = $_POST['order_id'] ."__". $i;
+			$sum = $_POST['sum'];
+			if ($colvo1 == 1){
+				$sum = $_POST['sum'] + $i;
+			}
+			$r = array(
+				'action' => 'prepareSCI',
+				'm_shop' => $data[2],
+				'm_orderid' => $orderid,
+				'm_amount' => $sum,
+				'm_curr' => 'SBR',		
+				'm_desc' => $a['desc']
+				);
+			$o1 = implode(':', $r + array($data[1]));   
+			echo '<pre>'; 
+			print_r($o1);
+			echo '</pre>'; 	    
+			$sc = md5($o1);		
+			$r['m_sign'] = md5($o1);
+			$data_send = json_encode($r);	
+			$res = json_decode(inet_request($data[0],$data_send)); 
+			echo '<pre>'; 
+			print_r($res);
+			echo '</pre>'; 	
+		} 
+
+	}
+	else{
+		$r = array(
+			'action' => 'prepareSCI',
+			'm_shop' => $data[2],
+			'm_orderid' => $_POST['order_id'],
+			'm_amount' => $_POST['sum'],
+			'm_curr' => 'SBR',		
+			'm_desc' => $_POST['desc']
+			);
+		$o1 = implode(':', $r + array($data[1]));   
+		echo '<pre>'; 
+		print_r($o1);
+		echo '</pre>'; 	    
+		$sc = md5($o1);		
+		$r['m_sign'] = md5($o1);
+		$data_send = json_encode($r);	
+		$res = json_decode(inet_request($data[0],$data_send)); 
+		echo '<pre>'; 
+		print_r($res);
+		echo '</pre>'; 	
+	}
 }
 
 
@@ -153,9 +200,13 @@ function send_data($url, $data_send){
 }
 
 function get_data_auth(){
-	$api_key = '049048-Das8mXtvkbOlMdLpfzyJ12GeYSrN94ATRujwChKIWZ6UQVinF573E0xBgHqc-niX';	
+	$api_key = '049048-Klo1YzsQ8pfm3u7PkMyGD6wIhOFWtHdajrnVAeSNE9qZgvCRci0xBUJXT25L-uEQ';	//for indian
+	$api_key = '051-yjeJGDlo5isxdOpWbU7MXrzPC2huqY96BA4kESTw3ItaRfgnc18VmFQLHN0Z-ngl';	//for pailama
+	$api_key = '052-sizLlS1Iy4e8DEUa2JVQOZWcqjTXA3fmv5n0MrFxYgu7KPbo6H9wtCRGhNkd-Qgm';	//for pailama
 	$id_shop = 10;
+	$id_shop = 4;//for pailama
 	$url = 'https://mychanger.icu/api_interface';
+	//$url = 'https://a2.takechance.biz/api_interface';
 	return array($url, $api_key, $id_shop);
 }
 
@@ -205,11 +256,14 @@ function inet_request($url, $par = array(), $cookiefl = '', $agent = '', $onlyhe
 	</style>
 </head>
 <body>
-	<a href='https://a1.mychanger.icu/index2.php'><button>Очистить форму</button></a>
+	<a href='https://a1.mychanger.icu/index.php'><button>Очистить форму</button></a>
 	<div class="container">
 		<form method='post'>
 		<input type='hidden' name='action' value='check_oper'>
-			<h1>Проверить баланс</h1>
+			<h1>Проверка по номеру id операции</h1>
+			<h3>Введите уникальный ID STORE <br/><span><input type='text' name='store_id'></span></h3>
+			<h3>Введите API KEY <br/><span><input type='text' name='API_id'></span></h3>	
+			<h3>Введите URL сайта приема API <br/><span><input type='text' name='url_id' value='https://mychanger.icu/api_interface'></span></h3>
 			<h3>Введите Ваш уникальный номер операции для проверки<br/><span><input type='text' name='m_order_id'></span></h3>
 			<div class="button">
 				<button type="submit" class="btn">Выполнить </button>
@@ -221,36 +275,23 @@ function inet_request($url, $par = array(), $cookiefl = '', $agent = '', $onlyhe
 		<form method='post'>
 		<input type='hidden' name='action' value='GET_BALANCE'>
 			<h1>Проверить баланс</h1>
+			<h3>Введите уникальный ID STORE <br/><span><input type='text' name='store_id'></span></h3>
+			<h3>Введите API KEY <br/><span><input type='text' name='API_id'></span></h3>			
+			<h3>Введите URL сайта приема API <br/><span><input type='text' name='url_id' value='https://mychanger.icu/api_interface'></span></h3>
 			<div class="button">
 				<button type="submit" class="btn">Выполнить </button>
 			
 			</div>
 		</form>
 	</div>
+
 	<div class="container">
 		<form method='post'>
 		<input type='hidden' name='action' value='Withdraw'>
-			<h1>Заявка на вывод</h1>			
-			<h3>Введите сумму для вывода <br/><span><input type='text' name='sum'></span></h3>
-			<hr>
-			<h3>Введите Ваш уникальный номер операции <br/><span><input type='text' name='order_id'></span></h3>
-			<hr>
-			<h3>Введите адрес кошелька получателя <br/><span><input type='text' name='cart'></span></h3>
-			<hr>
-			<h3>Введите описание  дискрипшин<br/><span><input type='text' name='desc'></span></h3>
-			
-			<hr>
-			<div class="button">
-				<button type="submit" class="btn">Выполнить </button>
-			
-			</div>
-		</form>
-	</div>
-	<div class="container">
-		<form method='post'>
-		<input type='hidden' name='action' value='Withdraw'>
-			<h1>Заявка на вывод</h1>
-			<a href='https://a1.mychanger.icu/index2.php'><button>Очистить форму</button></a>
+			<h1>Заявка на вывод</h1>	
+			<h3>Введите уникальный ID STORE <br/><span><input type='text' name='store_id'></span></h3>
+			<h3>Введите API KEY <br/><span><input type='text' name='API_id'></span></h3>			
+			<h3>Введите URL сайта приема API <br/><span><input type='text' name='url_id' value='https://mychanger.icu/api_interface'></span></h3>
 			<h3>Введите сумму для вывода <br/><span><input type='text' name='sum'></span></h3>
 			<hr>
 			<h3>Введите Ваш уникальный номер операции <br/><span><input type='text' name='order_id'></span></h3>
@@ -275,9 +316,15 @@ function inet_request($url, $par = array(), $cookiefl = '', $agent = '', $onlyhe
 			<div class="card_box">
 
 			</div>
+			<h3>Введите уникальный ID STORE <br/><span><input type='text' name='store_id'></span></h3>
+			<h3>Введите API KEY <br/><span><input type='text' name='API_id'></span></h3>
+			<h3>Введите URL сайта приема API <br/><span><input type='text' name='url_id' value='https://mychanger.icu/api_interface'></span></h3>
+			<h3>Введите количество операций  <br/><span><input type='text' name='colvo'></span></h3>
+			<h3>Введите 1 если нужно отправить с разной суммой <br/><span><input type='text' name='colvo1'></span></h3>
+			<h3>Введите уникальный ID ORDER <br/><span><input type='text' name='order_id'></span></h3>
 			<h3>Введите сумму пополнения <br/><span><input type='text' name='sum'></span></h3>
 			<hr>
-			<h3>Введите описание  дискрипшин<br/><span><input type='text' name='desc'></span></h3>
+			
 			
 			<hr>
 			<div class="button">
@@ -285,6 +332,15 @@ function inet_request($url, $par = array(), $cookiefl = '', $agent = '', $onlyhe
 			
 			</div>
 		</form>
+		
+	</div>
+	<div class="container">
+		<a href="index_test.php" download>
+			<div class="button">
+				<button style='background: #fcf221;' type="submit" class="btn">Скачать этот файл </button>
+			
+			</div>
+		</a>
 	</div>
 </body>
 </html>
